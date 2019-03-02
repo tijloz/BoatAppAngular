e45e451db25ae67bb2d4b0b86d8500a9cfd55e5a
tijloz <homerl3@aston.ac.uk>
2019-03-02 23:05:19 +0000

    Added a working clock and removed react dependency

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,13 +10,17 @@ import { SettingsComponent } from './settings/settings.component';
 import { ProtectionComponent } from './protection/protection.component';
 import { LightDetailComponent } from './light-detail/light-detail.component';
 import { HomeComponent } from './home/home.component';
-import React, { Component } from 'react';
-import Clock from 'react-clock';
 import { LightEditComponent } from './light-edit/light-edit.component';
 import { ProtectionEditComponent } from './protection-edit/protection-edit.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
 
 import 'hammerjs';
+import { MessagesComponent } from './messages/messages.component';
+import { DigitalClockComponent } from './digital-clock/digital-clock.component';
+import { UtilService } from './shared/services/util/util.service';
 
 
 @NgModule({
@@ -29,15 +33,21 @@ import 'hammerjs';
     HomeComponent,
     LightEditComponent,
     ProtectionEditComponent,
+    MessagesComponent,
+    DigitalClockComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
     MatTabsModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(
+    InMemoryDataService, { dataEncapsulation: false }
+    )
   ],
-  providers: [],
+  providers: [UtilService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
