e777cba5e11bcb0f84602b8a5e404a7b93b681b0
tijloz <homerl3@aston.ac.uk>
2019-03-09 02:08:39 +0000

    pushed main js 	new file:   .vs/ProjectSettings.json 	new file:   .vs/VSWorkspaceState.json 	new file:   .vs/boatInterface/v15/.suo 	new file:   .vs/config/applicationhost.config 	new file:   .vs/slnx.sqlite 	new file:   main.js 	new file:   src/.vs/ProjectSettings.json 	new file:   src/.vs/VSWorkspaceState.json 	new file:   src/.vs/config/applicationhost.config 	new file:   src/.vs/slnx.sqlite 	new file:   src/.vs/src/v15/.suo 	modified:   src/app/home/home.component.html 	new file:   src/main.js.map

---
+++ main.js
@@ -0,0 +1,58 @@
+const electron = require('electron');
+const SerialPort = require('serialport');
+const {app, BrowserWindow, ipcMain} = electron;
+const os = require('os');
+const osPlatform = os.platform();
+const port = new SerialPort(osSwap(), {
+  baudRate: 9200
+});
+
+function osSwap() {
+  switch (osPlatform) {
+    case 'win32':
+      return 'com3';
+      break;
+    case 'linux':
+      return '/dev/ttyACM0';
+  }
+}
+
+// Listen for app to be ready
+app.on('ready', function () {
+
+    /*      Create the main window      */
+    let mainWindow = new BrowserWindow({
+      show: false,
+      frame: true,
+      //kiosk: true,
+      //alwaysOnTop: true
+    });
+
+    // and load the index.html of the app.
+    mainWindow.loadFile('./dist/boatInterface/index.html');
+
+    // open dev tools
+    mainWindow.webContents.openDevTools();
+
+    // Stop visual flashing
+    mainWindow.once('ready-to-show', () => {
+      mainWindow.show()
+    });
+
+    // Quit app when closed
+    mainWindow.on('closed', function () {
+      mainWindow = null;
+      app.quit();
+    })
+  }
+);
+
+ipcMain.on('lightChannel', function (event, light) {
+  console.log(`Light state is: ${light.lightState}`);
+
+  if (light.lightState) {
+    port.write('ON' + '\n');
+  } else {
+    port.write('OFF' + '\n');
+  }
+});
